63dc092df1e3cfcba683486a23afe80580190553
KielAnthony <144108095+KielAnthony@users.noreply.github.com>
2023-11-26 00:48:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Set a specific secret number for demonstration
-const secretNumber = 17 ;
+// Set an initial secret number
+let secretNumber = generateRandomNumber();
+
+function generateRandomNumber() {
+  // Generate a random number between 1 and 100
+  return Math.floor(Math.random() * 100) + 1;
+}
 
 function checkGuess() {
   // Get the user's guess
@@ -8,6 +13,9 @@ function checkGuess() {
   // Check if the guess is correct
   if (userGuess == secretNumber) {
     document.getElementById("result").innerHTML = "Congratulations! You guessed the correct number!";
+    
+    // Change the secretNumber for the next round
+    secretNumber = generateRandomNumber();
   } else if (userGuess < secretNumber) {
     document.getElementById("result").innerHTML = "Too low! Try again.";
   } else {
