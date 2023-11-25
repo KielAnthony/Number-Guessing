3f8de13408947c5be3afc495b4cdf0c66aed849f
KielAnthony <144108095+KielAnthony@users.noreply.github.com>
2023-11-26 00:37:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Set a specific secret number for demonstration
+const secretNumber = 37 ;
+
+function checkGuess() {
+  // Get the user's guess
+  const userGuess = document.getElementById("guessInput").value;
+
+  // Check if the guess is correct
+  if (userGuess == secretNumber) {
+    document.getElementById("result").innerHTML = "Congratulations! You guessed the correct number!";
+  } else if (userGuess < secretNumber) {
+    document.getElementById("result").innerHTML = "Too low! Try again.";
+  } else {
+    document.getElementById("result").innerHTML = "Too high! Try again.";
+  }
+}
